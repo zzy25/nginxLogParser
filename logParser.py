5f7362ea5fcee488e73f0441f9a308e20fcd010d
Diego Ragazzi <d@ragazzid.com.br>
2015-08-18 18:44:57 -0300

    Preciso ver que mudancas fazer para retirar dados duplicados
    	modificado: logParser.py

--- logParser.py
+++ logParser.py
@@ -1,6 +1,8 @@
 import re
+import itertools
+import operator
 
-filename = "sportcarveiculos-access"
+filename = "example"
 logFile = open(filename)
 data = logFile.read()
 
@@ -28,33 +30,53 @@ def getAllErrorsCode():
     return re.findall( r'(\"GET\ [\\\/\\\ \w\.]+\"\ [0-9]{3})', data)
 
 def getErrorsCode():
-		listAllErros = getAllErrorsCode()
-		e400 = []
-		e500 = []
-		for a in listAllErros:
-				if bool(re.search(r'4[0-9]{2}', a)):
-						e400.append(re.findall(r'4[0-9]{2}', a))
-				if bool(re.search(r'5[0-9]{2}', a)):
-						e500.append(re.findall(r'5[0-9]{2}', a))
+	listAllErros = getAllErrorsCode()
+	e400 = []
+	e500 = []
+	for a in listAllErros:
+		if bool(re.search(r'4[0-9]{2}', a)):
+			e400.append(re.findall(r'4[0-9]{2}', a))
+			if bool(re.search(r'5[0-9]{2}', a)):
+				e500.append(re.findall(r'5[0-9]{2}', a))
 
-		return [e400,e500]
+	return [e400,e500]
 
 def getAccessedUrl():
-    return 0
+	return re.findall(r'http://[\w\:\/\.]+', data)
+
+def getMostAccessedUrl(L):
+    # get an iterable of (item, iterable) pairs
+    SL = sorted((x, i) for i, x in enumerate(L))
+    # print 'SL:', SL
+    groups = itertools.groupby(SL, key=operator.itemgetter(0))
+    # auxiliary function to get "quality" for an item
+    def _auxfun(g):
+        item, iterable = g
+        count = 0
+        min_index = len(L)
+        for _, where in iterable:
+            count += 1
+            min_index = min(min_index, where)
+        # print 'item %r, count %r, minind %r' % (item, count, min_index)
+        return count, -min_index
+    # pick the highest-count/earliest item
+    def f7(seq):
+        seen = set()
+        seen_add = seen.add
+        return [ x for x in seq if not (x in seen or seen_add(x))]
+    return f7(max(groups, key=_auxfun)[0])
 
 def getNumberOfError(errorCode):
-		listAllErrors = getAllErrorsCode()
-		errors = []
-		for a in listAllErrors:
-				if bool(re.search(r"errorCode", a)):
-						errors.append(re.findall(r"errorCode", a))
+    listAllErrors = getAllErrorsCode()
+    errors = []
+    for a in listAllErrors:
+        if bool(re.search(r'%s' % errorCode, a)):
+            errors.append(re.findall(r'errorCode', a))
 
-		return len(errors)
-
-def getMostAccessedUrl():
-    return 0
+    return len(errors)
 
 
-b = getNumberOfError(408)
+b = getAccessedUrl();
+c = getMostAccessedUrl(b)
 print (b)
 
